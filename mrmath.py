8406e5c972362a209103196dc68eb15af39a5e79
rich1126 <31447594+rich1126@users.noreply.github.com>
2019-08-01 13:07:59 -0700

    Add files via upload

--- mrmath.py
+++ mrmath.py
@@ -1,1 +1,5 @@
-from Classes import *
+from Classes.frac import *
+from Classes.complexNumbers import *
+from Classes.vector2D import *
+from Classes.functions import *
+from Classes.vector3D import *
